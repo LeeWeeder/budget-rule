0e4ba81236c632b7324f634a6798a678aed1fd5c
LeeWeeder <ljmaq239@gmail.com>
2023-09-08 11:24:14 +0800

    Add record item divider

--- app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/components/RecordItem.kt
+++ app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/components/RecordItem.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ljmaq.budgetrule.features.record.domain.model.Record
 import com.ljmaq.budgetrule.features.record.presentation.records.util.Formatter
@@ -23,31 +25,47 @@ fun RecordItem(
     modifier: Modifier = Modifier,
     onDeleteClick: () -> Unit
 ) {
-    Box(
-        modifier = modifier.background(MaterialTheme.colorScheme.surface)
-    ) {
-        Row(
-            modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+    Column {
+        Box(
+            modifier = modifier.background(MaterialTheme.colorScheme.surface)
         ) {
-            Column(
-                modifier = Modifier.padding(start = 16.dp)
+            Row(
+                modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = Formatter.formatCurrency(record.amount),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = if (record.amount.toDouble() < 0) {
-                        Color(0xFFCC0202)
-                    } else {
-                        Color(0xFF4BB543)
-                    },
-                    maxLines = 1
-                )
-                Text(text = Formatter.formatDate(record.timestamp))
+                Column(
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(
+                        text = Formatter.formatCurrency(record.amount),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = if (record.amount.toDouble() < 0) {
+                            Color(0xFFCC0202)
+                        } else {
+                            Color(0xFF4BB543)
+                        },
+                        maxLines = 1
+                    )
+                    Text(text = Formatter.formatDate(record.timestamp))
+                }
             }
+        }
+        Divider()
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    Column {
+        RecordItem(record = Record(timestamp = 1L, amount = "100.00", isExpenses = false)) {
+
+        }
+        RecordItem(record = Record(timestamp = 1L, amount = "100.00", isExpenses = false)) {
+
         }
     }
 }
