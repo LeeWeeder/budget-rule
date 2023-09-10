530b04566cfd2e9b8554ce39e1254fffecce9027
LeeWeeder <ljmaq239@gmail.com>
2023-09-10 18:58:14 +0800

    Add indicator as background and text color

--- app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/components/TabItem.kt
+++ app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/components/TabItem.kt
@@ -1,5 +1,6 @@
 package com.ljmaq.budgetrule.features.record.presentation.records.add_record.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
@@ -7,13 +8,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TabItem(selected: Boolean, onClick: () -> Unit, text: String) {
     Tab(
-        selected = selected, onClick = onClick, modifier = Modifier.clip(MaterialTheme.shapes.extraLarge)
+        selected = selected, onClick = onClick, modifier = Modifier
+            .clip(MaterialTheme.shapes.extraLarge)
+            .background(if (selected) MaterialTheme.colorScheme.secondary else Color.Transparent)
     ) {
-        Text(text = text.uppercase(), modifier = Modifier.padding(15.dp))
+        Text(text = text.uppercase(), modifier = Modifier.padding(15.dp), color = if (selected) MaterialTheme.colorScheme.onSecondary else MaterialTheme.colorScheme.primary)
     }
 }
