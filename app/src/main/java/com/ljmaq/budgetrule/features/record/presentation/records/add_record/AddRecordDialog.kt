3e2c7d169ace5d015736194592b88b25deff2828
LeeWeeder <ljmaq239@gmail.com>
2023-09-10 15:24:37 +0800

    Implement type tab design

--- app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/AddRecordDialog.kt
+++ app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/AddRecordDialog.kt
@@ -1,16 +1,22 @@
 package com.ljmaq.budgetrule.features.record.presentation.records.add_record
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -22,6 +28,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -60,7 +70,7 @@ fun AddRecordDialog(
 
     Dialog(
         onDismissRequest = {
-                           recordsViewModel.onEvent(RecordsEvent.CancelCreateRecord)
+            recordsViewModel.onEvent(RecordsEvent.CancelCreateRecord)
         },
         properties = DialogProperties(
             usePlatformDefaultWidth = false,
@@ -68,45 +78,64 @@ fun AddRecordDialog(
         )
     ) {
         Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }, topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = {
+                    recordsViewModel.onEvent(RecordsEvent.CancelCreateRecord)
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Close,
+                        contentDescription = "Close icon"
+                    )
+                }
+                IconButton(onClick = {
+                    viewModel.onEvent(AddRecordEvent.SaveRecord)
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.Check,
+                        contentDescription = "Save icon"
+                    )
+                }
+            }
         }) { paddingValues ->
             Column(
                 modifier = Modifier
                     .padding(paddingValues)
                     .fillMaxSize()
             ) {
-                Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                    IconButton(onClick = {
-                        viewModel.onEvent(AddRecordEvent.SaveRecord)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Check,
-                            contentDescription = "Save icon"
-                        )
-                    }
-                    IconButton(onClick = {
-                        recordsViewModel.onEvent(RecordsEvent.CancelCreateRecord)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Close,
-                            contentDescription = "Close icon"
-                        )
-                    }
-                }
-                TabRow(selectedTabIndex = tabState.index) {
+                Spacer(modifier = Modifier.height(12.dp))
+                TabRow(selectedTabIndex = tabState.index,
+                    modifier = Modifier
+                        .padding(vertical = 4.dp, horizontal = 8.dp)
+                        .clip(MaterialTheme.shapes.extraLarge)
+                        .border(
+                            BorderStroke(Dp.Hairline, MaterialTheme.colorScheme.outline),
+                            MaterialTheme.shapes.extraLarge
+                        ),
+                    indicator = {}, divider = {}) {
+                    val secondaryColor = MaterialTheme.colorScheme.secondary
+                    val onSecondaryColor = MaterialTheme.colorScheme.onSecondary
+
+                    val modifier = Modifier.padding(15.dp)
                     Tab(
                         selected = !typeIsExpenses, onClick = {
                             viewModel.onEvent(
                                 AddRecordEvent.ChangeRecordType
                             )
-                        }) {
-                        Text(text = "Income")
+                        }, modifier = Modifier.background(if (!typeIsExpenses) secondaryColor else Color.Transparent)
+                    ) {
+                        Text(text = "INCOME", modifier = modifier, color = if (!typeIsExpenses) onSecondaryColor else secondaryColor)
                     }
                     Tab(selected = typeIsExpenses, onClick = {
                         viewModel.onEvent(
                             AddRecordEvent.ChangeRecordType
                         )
-                    }) {
-                        Text(text = "Expenses")
+                    }, modifier = Modifier.background(if (typeIsExpenses) secondaryColor else Color.Transparent)) {
+                        Text(text = "EXPENSES", modifier = modifier, color = if (typeIsExpenses) onSecondaryColor else secondaryColor)
                     }
                 }
                 Row(modifier = Modifier.fillMaxWidth()) {
