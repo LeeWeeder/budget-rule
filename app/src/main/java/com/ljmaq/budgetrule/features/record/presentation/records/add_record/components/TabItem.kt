a5265e8ea3b7982d69535ba5af9627bebafdf5ed
LeeWeeder <ljmaq239@gmail.com>
2023-09-10 18:46:56 +0800

    Design tab row

--- app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/components/TabItem.kt
+++ app/src/main/java/com/ljmaq/budgetrule/features/record/presentation/records/add_record/components/TabItem.kt
@@ -1,17 +1,19 @@
 package com.ljmaq.budgetrule.features.record.presentation.records.add_record.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TabItem(selected: Boolean, onClick: () -> Unit, text: String) {
     Tab(
         selected = selected, onClick = onClick, modifier = Modifier.clip(MaterialTheme.shapes.extraLarge)
     ) {
-        Text(text = text.uppercase())
+        Text(text = text.uppercase(), modifier = Modifier.padding(15.dp))
     }
 }
